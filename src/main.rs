fcf66249a775edb51bf6da76ffcce1989441130b
tim <tmsteem82@gmail.com>
2021-07-13 07:25:59 +0200

    implement connection to binance websocket stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use tungstenite::connect;
+use url::Url;
+
+static BINANCE_WS_API: &str = "wss://stream.binance.com:9443";
 fn main() {
-    println!("Hello, world!");
+    let binance_url = format!("{}/ws/ethbtc@depth5@100ms", BINANCE_WS_API);
+    let (mut socket, response) =
+        connect(Url::parse(&binance_url).unwrap()).expect("Can't connect.");
+
+    println!("Connected to binance stream.");
+    println!("HTTP status code: {}", response.status());
+    println!("Response headers:");
+    for (ref header, ref header_value) in response.headers() {
+        println!("- {}: {:?}", header, header_value);
+    }
 }
